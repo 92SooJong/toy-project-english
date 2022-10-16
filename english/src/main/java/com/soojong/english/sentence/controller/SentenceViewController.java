b766ea1fe75265243604530694d03788e08a2fed
글로벌개발팀/하수종 <joey.ha@musinsa.com>
2022-10-17 00:01:18 +0900

    create project

---
+++ english/src/main/java/com/soojong/english/sentence/controller/SentenceViewController.java
@@ -0,0 +1,14 @@
+package com.soojong.english.sentence.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class SentenceViewController {
+
+    @GetMapping
+    public ModelAndView main() {
+        return new ModelAndView("/sentence/input_sentence");
+    }
+}
