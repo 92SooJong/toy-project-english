9f32740162d282509549d3fe1809fa974f367ade
하수종 <hsz3855@gmail.com>
2022-11-03 00:30:07 +0900

    Getter 적용

--- english/src/main/java/com/soojong/english/common/entity/BaseTimeAuditingEntity.java
+++ english/src/main/java/com/soojong/english/common/entity/BaseTimeAuditingEntity.java
@@ -7,7 +7,9 @@ import javax.persistence.MappedSuperclass;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import lombok.Getter;
 
+@Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class BaseTimeAuditingEntity {
