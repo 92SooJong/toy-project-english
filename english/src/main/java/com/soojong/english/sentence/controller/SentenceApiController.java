8123ad7ddc245302e413a6b95f379efb749b302b
하수종 <hsz3855@gmail.com>
2022-11-06 23:46:32 +0900

    feat: apply required field validation

--- english/src/main/java/com/soojong/english/sentence/controller/SentenceApiController.java
+++ english/src/main/java/com/soojong/english/sentence/controller/SentenceApiController.java
@@ -1,7 +1,13 @@
 package com.soojong.english.sentence.controller;
 
+
+import javax.validation.Valid;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.soojong.english.sentence.dto.SentenceCreateRequestDTO;
 import com.soojong.english.sentence.service.SentenceService;
@@ -10,6 +16,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@Validated
 @RestController
 @RequiredArgsConstructor
 public class SentenceApiController {
@@ -17,7 +24,7 @@ public class SentenceApiController {
     private final SentenceService sentenceService;
 
     @PostMapping("/sentence/api/v1")
-    public Long addSentence(@RequestBody SentenceCreateRequestDTO dto) {
+    public Long addSentence(@Valid @RequestBody SentenceCreateRequestDTO dto) {
 
         SentenceRegistrationRequestVO registrationRequestVO = SentenceRegistrationRequestVO.builder()
             .koreanSentence(dto.getKoreanSentence())
--- english/src/main/java/com/soojong/english/sentence/dto/SentenceCreateRequestDTO.java
+++ english/src/main/java/com/soojong/english/sentence/dto/SentenceCreateRequestDTO.java
@@ -1,18 +1,28 @@
 package com.soojong.english.sentence.dto;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Builder.Default;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 
-@Setter
 @Getter
+@Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class SentenceCreateRequestDTO {
 
+    @NotBlank(message = "Please Insert English Sentence")
     private String englishSentence;
+
+    @NotBlank(message = "Please Insert Korean Sentence")
     private String koreanSentence;
 
 }
