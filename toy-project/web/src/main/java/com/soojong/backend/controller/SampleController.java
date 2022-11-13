18e8c2a075f7ed6ede74f3558edd43e559278398
하수종 <hsz3855@gmail.com>
2022-11-13 17:52:42 +0900

    change module structure

---
+++ toy-project/web/src/main/java/com/soojong/backend/controller/SampleController.java
@@ -0,0 +1,24 @@
+package com.soojong.backend.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+    @Autowired
+    private RedisTemplate<String, String> template;
+
+    @GetMapping("/sample")
+    public String getSample() {
+
+        Integer append = template.opsForValue().append("ha", "soo jong");
+        System.out.println(append);
+
+        return null;
+    }
+
+
+}
